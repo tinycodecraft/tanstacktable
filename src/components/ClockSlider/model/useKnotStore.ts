8d5d917648b36f113d6604b8098c1105993f04b6
tinycodecraft <raymond.cw.yau@gmail.com>
2023-12-07 09:04:34 +0800

    add fix on inbound code

--- src/components/ClockSlider/model/useKnotStore.ts
+++ src/components/ClockSlider/model/useKnotStore.ts
@@ -17,22 +17,42 @@ export const useKnotStore = create<KnotState>((set, get) => ({
         }
 
         if (draft.knots.length < draft.count) {
+          
           draft.knots.push(payload)
         } else {
-          draft.knots.splice(0,1)
-          draft.knots.push(payload)
+          draft.knots.splice(0, 1)
+          draft.knots.push(payload)          
         }
         isOkay && isOkay(true)
       }),
     ),
   peek: (payload) => {
-    if (payload < get().knots.length) return get().knots[payload]
+    const itknots = get().knots
+    const found = itknots.find((y) => y.index == payload)
+    if (found) return found
+
     return null
   },
   isInbound: (payload) => {
     const itknots = get().knots
     if (itknots.length > 1) {
       const lastknot = itknots[itknots.length - 1]
+      const prevknot = itknots[itknots.length-2]
+      const angleInDifflast =  payload.angleDeg- lastknot.angleDeg
+      const angleInDiffprev =  lastknot.angleDeg- prevknot.angleDeg
+
+      console.log(`load : ${payload.angleDeg} last: ${lastknot.angleDeg} prev: ${prevknot.angleDeg}`)
+
+      if(angleInDifflast< 0 && angleInDifflast+360 < 150)
+      {
+        
+        return false
+      }
+      if( angleInDifflast > 150 && angleInDiffprev < 0)
+      {
+        return true;
+      }
+
       return lastknot.angleDeg > payload.angleDeg
     }
     return false
